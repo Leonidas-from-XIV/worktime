df43eb1be092ae1b8a07507157ab1fede1e4eef2
Marek Kubica <marek@xivilization.net>
2013-06-22 11:59:14 +0200

    Added initial working version of work time counter

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject worktime "0.1.0-SNAPSHOT"
+  :description "Calculates my working hours"
+  :url "https://github.com/Leonidas-from-XIV/worktime"
+  :license {:name "GNU General Public License"
+            :url "https://www.gnu.org/licenses/gpl-3.0.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/tools.cli "0.2.2"]
+                 [clj-time "0.5.1"]]
+  :main worktime.core)
---
+++ src/worktime/core.clj
@@ -0,0 +1,47 @@
+(ns worktime.core)
+(use 'clojure.java.io)
+(use '[clojure.tools.cli :only [cli]])
+(use '[clojure.string :only [split join]])
+(use 'clj-time.core)
+(use 'clj-time.format)
+
+(defn get-lines [filename]
+  (with-open [rdr (reader filename)]
+    (doall (line-seq rdr))))
+
+(def time-formatter (formatter "dd.MM.YYYY' 'HH:mm"))
+(def cli-formatter (formatter "dd.MM.YYYY"))
+
+(defn parse-line [line]
+  (let [pieces (split line #"\s")
+        start-string (join " " [(pieces 0) (pieces 1)])
+        stop-string (join " " [(pieces 0) (pieces 3)])
+        start-time (parse time-formatter start-string)
+        stop-candidate (parse time-formatter stop-string)
+        stop-time (if (and (= (hour stop-candidate) 23) (= (minute stop-candidate) 59))
+                    (plus stop-candidate (minutes 1)) stop-candidate)
+        duration (interval start-time stop-time)]
+    [(date-time (year start-time) (month start-time) (day start-time)) (in-minutes duration)]))
+
+(defn parse-cli-date [date]
+  (parse cli-formatter date))
+
+(defn -main
+  "I don't do a whole lot."
+  [& args]
+  (let [[opts args usage] (cli args
+                               ["-f" "--file" "Filename" :default "worktime.txt"]
+                               ["-s" "--start" "Start" :default (date-time 1 1 1) :parse-fn parse-cli-date]
+                               ["-e" "--end" "End" :default (date-time 9999 12 31) :parse-fn parse-cli-date]
+                               ["-h" "--help" "Print usage" :flag true])]
+    (when (opts :help)
+      (println usage)
+      (System/exit 0))
+    (let [entries (map parse-line (get-lines (opts :file)))
+          relevant (filter #(within? (opts :start) (opts :end) (% 0)) entries)
+          worktime (reduce (fn [acc e] (+ acc (e 1))) 0 relevant)
+          hours (quot worktime 60)
+          minutes (mod worktime 60)]
+      (if (= minutes 0)
+        (printf "Worked %d hours\n" hours)
+        (printf "Worked %d hours, %d minutes\n" hours minutes)))))
